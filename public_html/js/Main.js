80978b84adab59b5ec0f20313575cd5ba031c6ee
anna <anna@127.0.53.53>
2015-05-21 21:58:53 +0300

    Изменение структуры главного файла

--- public_html/js/Main.js
+++ public_html/js/Main.js
@@ -1,81 +1,88 @@
 /* global THREE, CONFIG, Detector */
 
-var scene;    
-var camera;  
-var renderer; 
-var game;
-var frameCounter = 0;
+function GameScene (game) {
+    this.game = game;
+}
+
+GameScene.prototype = {
+    constructor : GameScene,
+    
+    frameCounter : 0,
+    
+    init : function () {
+        this.scene = createScene();
+        this.camera = createCamera();
+        this.renderer = createRenderer();
+        
+        this.addEvents(this.renderer, this.game);
+        
+        var gameContainer = getGameContainer();
+        gameContainer.appendChild(this.renderer.domElement);
+        
+        this.scene.add(createGround());
+        this.scene.add(createMainLight()); 
+        
+        this.snakeImage = new SnakeImage(this.game.snake, this.scene);
+        
+        this.fireflyImages = new Array(CONFIG.countOfFireflies);
+        for (var i = 0; i < this.fireflyImages.length; i++) {
+            this.fireflyImages[i] = new FireflyImage(this.game.fireflies[i], this.scene);
+        }
+        
+        this.scoreHandler = new UpdateScoreHandler(this.game, document.getElementById(CONFIG.scoreLabelName));
+        this.gameOverHandler = new GameOverHandler(this.game, document.getElementById(CONFIG.gameOverLabelName));
+        
+        this.updateRendererSize();
+        this.draw();
+    },
+    
+    draw : function () {
+        if (this.frameCounter === (CONFIG.snakeDelay * 1000000)) {
+            this.frameCounter = 0;
+        }
+        this.game.nextStep(this.frameCounter++);
+        requestAnimationFrame(this.draw.bind(this));
+        this.renderer.render(this.scene, this.camera);
+    },
+    
+    updateRendererSize : function () {
+        var gameContainer = getGameContainer();
+        var availableWidth = gameContainer.offsetWidth;
+        var availableHeight = gameContainer.offsetHeight;
+        var height = availableHeight;
+        var width = Math.round(height * CONFIG.rendererRatio);
+        if (width > availableWidth) {
+            width = availableWidth;
+            height = Math.round(width * (1 / CONFIG.rendererRatio));
+        }
+        this.renderer.setSize(width, height);
+    },
+    
+    addEvents : function (renderer, game) {
+        window.addEventListener("load", function(event) {
+            window.onresize = this.updateRendererSize;
+            document.addEventListener('keydown', new KeyDownHandler(game));
+            renderer.domElement.addEventListener('touchstart', new TouchStartHandler(game));
+            var playButtonHandler = new PlayButtonHandler(game);
+            document.getElementById(CONFIG.playButtonName).onclick = playButtonHandler.handleEvent.bind(playButtonHandler);
+            var pauseButtonHandler = new SuspendButtonHandler(game);
+            document.getElementById(CONFIG.pauseButtonName).onclick = pauseButtonHandler.handleEvent.bind(pauseButtonHandler);
+            var stopButtonHandler = new StopButtonHandler(game);
+            document.getElementById(CONFIG.stopButtonName).onclick = stopButtonHandler.handleEvent.bind(stopButtonHandler);
+            document.getElementById(CONFIG.incSpeedButtonName).onclick = function () { CONFIG.decDelay(); };
+            document.getElementById(CONFIG.decSpeedButtonName).onclick = function () { CONFIG.incDelay(); };
+        });
+    }
+};
 
 function snakeGame () {
     if (!Detector.webgl) {
         Detector.addGetWebGLMessage();
     } else {
-        init();
+        var game = new Game();
+        var gameScene = new GameScene(game);
+        gameScene.init();
     }
-}
-
-function init () {
-    addEvents();    
-    
-    scene = createScene();
-    camera = createCamera();
-    renderer = createRenderer();
-    
-    var gameContainer = getGameContainer();
-    gameContainer.appendChild(renderer.domElement);
-
-    scene.add(createGround());
-    scene.add(createMainLight());   
-    
-    game = new Game();
-    var snakeImage = new SnakeImage(game.snake, scene);
-    var fireflyImages = new Array(CONFIG.countOfFireflies);
-    for (var i = 0; i < fireflyImages.length; i++) {
-        fireflyImages[i] = new FireflyImage(game.fireflies[i], scene);
-    }
-    var scoreHandler = new UpdateScoreHandler(game, document.getElementById(CONFIG.scoreLabelName));
-    var gameOverHandler = new GameOverHandler(game, document.getElementById(CONFIG.gameOverLabelName));
-
-    updateRendererSize();
-    draw();
-}
-
-function draw () {
-    if (frameCounter === (CONFIG.snakeDelay * 1000000)) {
-        frameCounter = 0;
-    }
-    game.nextStep(frameCounter++);
-    requestAnimationFrame(draw);
-    renderer.render(scene, camera);
-}
-
-function addEvents () {
-    window.addEventListener("load", function(event) {
-        window.onresize = updateRendererSize;
-        document.addEventListener('keydown', new KeyDownHandler(game));
-        renderer.domElement.addEventListener('touchstart', new TouchStartHandler(game));
-        var playButtonHandler = new PlayButtonHandler(game);
-        document.getElementById(CONFIG.playButtonName).onclick = playButtonHandler.handleEvent.bind(playButtonHandler);
-        var pauseButtonHandler = new SuspendButtonHandler(game);
-        document.getElementById(CONFIG.pauseButtonName).onclick = pauseButtonHandler.handleEvent.bind(pauseButtonHandler);
-        var stopButtonHandler = new StopButtonHandler(game);
-        document.getElementById(CONFIG.stopButtonName).onclick = stopButtonHandler.handleEvent.bind(stopButtonHandler);
-        document.getElementById(CONFIG.incSpeedButtonName).onclick = function () { CONFIG.decDelay(); };
-        document.getElementById(CONFIG.decSpeedButtonName).onclick = function () { CONFIG.incDelay(); };
-    });
-}
-
-function updateRendererSize () {
-    var gameContainer = getGameContainer();
-    var availableWidth = gameContainer.offsetWidth;
-    var availableHeight = gameContainer.offsetHeight;
-    var height = availableHeight;
-    var width = Math.round(height * CONFIG.rendererRatio);
-    if (width > availableWidth) {
-        width = availableWidth;
-        height = Math.round(width * (1 / CONFIG.rendererRatio));
-    }
-    renderer.setSize(width, height);
 }
 
 function createScene () {
